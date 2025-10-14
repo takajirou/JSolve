ca8200f69a17dfce7b7d0d61f40dfc9dcaa2e679
sojiro takagi <soujirou.takagi@gmail.com>
2025-10-14 16:24:36 +0900

    config設定変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ["example.com"],
+    },
 };
-
 export default nextConfig;
