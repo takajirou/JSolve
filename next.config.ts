8f229d5671998c9dcd6808fe4b13353d704de676
sojiro takagi <soujirou.takagi@gmail.com>
2026-02-05 14:08:05 +0900

    fix:swcminifyを削除

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
-    swcMinify: true,
+
     images: {
         domains: ["example.com"],
     },
